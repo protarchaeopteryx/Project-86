bb9d83e844b64c312cb935a3d55a661acfd76e3c
protarchaeopteryx <93206974+protarchaeopteryx@users.noreply.github.com>
2022-08-06 18:21:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas("myCanvas");
+ var x= document.getElementById("myAudio");
+
+function new_image(){
+	fabric.image.fromUrl('BiethdayImage.jpg' , function(Img) {
+        block_image_object = Img
+
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({
+            top:0 , 
+            left:0
+        });
+        canvas.add(block_image_object);
+    });
+}
+
+function playSound(){
+	document.getElementById("myAudio").play;
+}
